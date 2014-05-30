34871f376ca5a350b16a231f95540b803aacf235
Armando Andini <armando.andini@hotmail.com>
2014-05-30 19:41:53 -0300

    Newton interpolation calculator reimplemented.

--- bots/newton_calc.rb
+++ bots/newton_calc.rb
@@ -1,24 +1,26 @@
 module NewtonCalc
-  def self.cuadratic_approximation array, t
-    t += 2
 
-    a1 = array[-3]
-    a2 = array[-2]
-    a3 = array[-1]
-    b1 = a2 - a1
-    b2 = a3 - a2
-    b3 = (b2 - b1) / 2
+  #ts: valores de la variable independiente
+  #ys: valores de la imagen en t
+  #t: valor de la variable independiente para evaluar la aproximacion
+  def self.cuadratic_approximation ts, ys, t
+    t0,t1,t2 = ts
+    y0,y1,y2 = ys
 
-    prediccion = a1 + b1 * t + b3 * t * (t - 1)
+    b1 = (y1 - y0) / (t1 - t0)
+    b2 = (y2 - y1) / (t2 - t1)
+    b3 = (b2 - b1) / (t2 - t0)
+
+    prediccion = y0 + b1 * (t - t0) + b3 * (t - t0) * (t - t1)
   end
 
-  def self.linear_approximation array, t
-    t += 1
+#  def self.linear_approximation array, t
+#    t += 1
 
-    a1 = array[-2]
-    a2 = array[-1]
-    b1 = a2 - a1
+#    a1 = array[-2]
+#    a2 = array[-1]
+#    b1 = a2 - a1
 
-    prediccion = a1 + b1 * t
-  end
+#    prediccion = a1 + b1 * t
+#  end
 end
